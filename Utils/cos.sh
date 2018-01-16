59070b9c9922827b4d14fce8f3e6f2695fd56b6c
John Harrigan <jharriga@redhat.com>
2018-01-16 16:44:51 -0500

    uses vars.shinc now

--- Utils/cos.sh
+++ Utils/cos.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+# Bring in other script files
+myPath="${BASH_SOURCE%/*}"
+if [[ ! -d "$myPath" ]]; then
+    myPath="$PWD"
+fi
 
-#cosPath=/root/0.5.2
-cosPath=/root/0.4.2.c4
-function run1 {
-	f_name=$1
-	OUTPUT=$(sh $cosPath/cli.sh submit $f_name) &&
-	echo $OUTPUT
-	jobId=$(echo $OUTPUT |awk '{print $4}') &&
-	echo "ugur"
-	echo $jobId
-	echo "ugur"
+# Variables
+source "$myPath/../vars.shinc"
+
+# Functions
+source "$myPath/../Utils/functions.shinc"
+
+f_name=$1
+OUTPUT=$(sh $cosPath/cli.sh submit $f_name) &&
+echo $OUTPUT
+jobId=$(echo $OUTPUT |awk '{print $4}') &&
+echo "ugur"
+echo $jobId
+echo "ugur"
 #	echo "$cosPath/archive/$jobId-rep.144.obj.4m/$jobId-rep.144.obj.4m.csv"
-	exist=1
-	while [ $exist -eq 1 ];do
-	if grep "completed" $cosPath/archive/$jobId-write/$jobId-write.csv > /dev/null
-	then
-		exist=0
-	fi
-	sleep 30
-	done
-}
-
-run1 $1
+exist=1
+while [ $exist -eq 1 ];do
+    if grep "completed" $cosPath/archive/$jobId-write/$jobId-write.csv > /dev/null
+    then
+        exist=0
+    fi
+    sleep 30
+done
