d089e476f9239fbd9929aca107c7662316eeb68d
John Harrigan <jharriga@redhat.com>
2018-01-29 16:21:35 -0500

    changed cosPath to cosPATH

--- Utils/cos.sh
+++ Utils/cos.sh
@@ -13,18 +13,23 @@ source "$myPath/../vars.shinc"
 source "$myPath/../Utils/functions.shinc"
 
 f_name=$1
-OUTPUT=$(sh $cosPath/cli.sh submit $f_name) &&
+OUTPUT=$(sh $cosPATH/cli.sh submit $f_name) &&
 echo $OUTPUT
 jobId=$(echo $OUTPUT |awk '{print $4}') &&
-echo "ugur"
-echo $jobId
-echo "ugur"
-#	echo "$cosPath/archive/$jobId-rep.144.obj.4m/$jobId-rep.144.obj.4m.csv"
-exist=1
-while [ $exist -eq 1 ];do
-    if grep "completed" $cosPath/archive/$jobId-write/$jobId-write.csv > /dev/null
-    then
-        exist=0
+echo "COSbench jobID is: $jobId - Started at `get_time` "
+
+running=1
+csv_file="$cosPATH/archive/${jobId}-prepCluster/${jobId}-prepCluster.csv"
+while [ $running ]; do
+    if [ -f $csv_file ]; then
+        if grep "completed" $csv_file > /dev/null; then
+            running=0
+        else
+            echo "ERROR: COSbench job did not complete cleanly"
+            exit 1
+        fi
     fi
     sleep 30
 done
+
+echo "COSbench jobID: $jobId - Completed at `get_time` "
