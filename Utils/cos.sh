6314e31a6fb329da3c893970385bb0e78439795f
John Harrigan <jharriga@redhat.com>
2018-01-29 19:13:15 -0500

    changed IF tests

--- Utils/cos.sh
+++ Utils/cos.sh
@@ -20,16 +20,17 @@ echo "COSbench jobID is: $jobId - Started at `get_time` "
 
 running=1
 csv_file="$cosPATH/archive/${jobId}-prepCluster/${jobId}-prepCluster.csv"
-while [ $running ]; do
+while [ $running -eq 1 ]; do
     if [ -f $csv_file ]; then
-        if grep "completed" $csv_file > /dev/null; then
+        grep -q "completed" "$csv_file"
+        if [ $? -eq 0 ]; then
             running=0
         else
             echo "ERROR: COSbench job did not complete cleanly"
             exit 1
         fi
     fi
-    sleep 30
+    sleep 10
 done
 
 echo "COSbench jobID: $jobId - Completed at `get_time` "
