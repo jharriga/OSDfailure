db4e4efe6953230d4065d6b6995efdff4c4604bb
John Harrigan <jharriga@redhat.com>
2018-02-14 14:25:34 -0500

    changed while loop test

--- Utils/cos.sh
+++ Utils/cos.sh
@@ -19,18 +19,13 @@ jobId=$(echo $OUTPUT |awk '{print $4}') &&
 echo "COSbench jobID is: $jobId - Started at `get_time` "
 
 running=1
-csv_file="$cosPATH/archive/${jobId}-prepCluster/${jobId}-prepCluster.csv"
 while [ $running -eq 1 ]; do
-    if [ -f $csv_file ]; then
-        grep -q "completed" "$csv_file"
-        if [ $? -eq 0 ]; then
-            running=0
-        else
-            echo "ERROR: COSbench job did not complete cleanly"
-            exit 1
-        fi
+    run_path="$cosPATH/archive/$jobId-*"
+    if [ -d $run_path ]; then
+        running=0
+    else
+        sleep 10
     fi
-    sleep 10
 done
 
 echo "COSbench jobID: $jobId - Completed at `get_time` "
