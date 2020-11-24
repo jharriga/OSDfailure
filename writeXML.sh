f5938075e95d63fc929359cd2f4d3238813f2a32
John Harrigan <jharriga@redhat.com>
2020-11-24 08:51:46 -0500

    removed prepareXML file

--- writeXML.sh
+++ writeXML.sh
@@ -13,27 +13,8 @@ source "$myPath/vars.shinc"
 # Functions
 #source "$myPath/Utils/functions.shinc"
 
-
 echo "Creating COSbench XML workload files from settings in vars.shinc"
-# Phase 1: PREPARExml
-# backup the XML file if it exists
-if [ -f "${PREPARExml}" ]; then
-    mv "${PREPARExml}" "${PREPARExml}_bak"
-    echo "> ${PREPARExml} exists - moved to ${PREPARExml}_bak"
-fi
-# copy the Template and make edits
-# FCkeys_arr and FCvalues_arr defined in vars.shinc
-cp "${PREPAREtemplate}" "${PREPARExml}"
-
-let index=0
-for origValue in "${PCkeys_arr[@]}"; do
-    newValue="${PCvalues_arr[index]}"
-    sed -i "s/${origValue}/${newValue}/g" $PREPARExml
-    index=$(( $index + 1 ))
-done
-echo "> created COSbench workload file: ${PREPARExml}"
-
-# Phase 2: RUNTESTxml
+# RUNTESTxml
 # backup the XML file if it exists
 if [ -f "${RUNTESTxml}" ]; then
     mv "${RUNTESTxml}" "${RUNTESTxml}_bak"
